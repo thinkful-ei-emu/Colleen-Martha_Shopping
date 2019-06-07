9a0c1466745533d2a2508a20f160ccafe1f00a07
Martha Plantz <maplantz89@gmail.com>
2019-06-06 17:56:01 -0700

    failed attempt at getting this to work

--- index.js
+++ index.js
@@ -2,6 +2,69 @@
 // check and uncheck items on the list by clicking the "Check" button
 // permanently remove items from the list
 'use strict';
+/**
+ * add user text to list item when clicking 'Add item' button Or hitting return 
+ * store user text and add that text to the list when you click button or hit return 
+ */
+
+//when you click submit ... add text to list 
+$('#js-shopping-list-form').submit(event => {
+  event.preventDefault();
+  //this is the users text
+  const newEntry = $(event.currentTarget).find('#shopping-list-entry');
+  const testEntry = newEntry.val();
+
+  //adding user text to the list 
+  $('ul').append( `    
+<li>
+<span class="shopping-item">${testEntry}</span>
+<div class="shopping-item-controls">
+  <button class="shopping-item-toggle">
+    <span class="button-label">check</span>
+  </button>
+  <button class="shopping-item-delete">
+    <span class="button-label">delete</span>
+  </button>
+</div>
+</li>' `
+  );
+
+  //deleting the users text from the box
+  testEntry.val('');
+});
+
+// //when you hit return 
+// $('#js-shopping-list-form').keypress(event => {
+//   var key = event.which;
+//   if(key === 13){
+//     event.preventDefault();
+//     //this is the users text
+//     const newEntry = $(event.currentTarget).find('#shopping-list-entry');
+//     const testEntry = newEntry.val();
+  
+
+
+//     //adding user text to the list 
+//     $('ul').append( `    
+//   <li>
+//   <span class="shopping-item">${testEntry}</span>
+//   <div class="shopping-item-controls">
+//     <button class="shopping-item-toggle">
+//       <span class="button-label">check</span>
+//     </button>
+//     <button class="shopping-item-delete">
+//       <span class="button-label">delete</span>
+//     </button>
+//   </div>
+// </li>' `
+//     );
+
+
+//     //deleting the users text from the box
+//     testEntry.val('');
+//   }
+// });
+
 
 /**
  * when the 'check' button is CLICKED it will add the class .shopping-item__checked to
@@ -19,36 +82,13 @@ $('.shopping-item-toggle').click(event => {
 );
 
 /**
- * add user text to list item when clicking 'Add item' button Or hitting return 
- * store user text and add that text to the list when you click button or hit return 
+ * Remove item from list permanently when clicking button
  */
-
-//when you click submit ... add text to list 
-$('#js-shopping-list-form').submit(event => {
+ 
+$('.shopping-item-delete').click(event => {
   event.preventDefault();
-  //this is the users text
-  const newEntry = $(event.currentTarget).find('#shopping-list-entry');
-  const testEntry = newEntry.val();
-  console.log(newEntry);
-
-
-  //adding user text to the list 
-  $('ul').append( `    
-  <li>
-  <span class="shopping-item">${testEntry}</span>
-  <div class="shopping-item-controls">
-    <button class="shopping-item-toggle">
-      <span class="button-label">check</span>
-    </button>
-    <button class="shopping-item-delete">
-      <span class="button-label">delete</span>
-    </button>
-  </div>
-</li>' `
-  );
-
-  //deleting the users text from the box
-  newEntry.val('');
-
-});
+  console.log(event.currentTarget);
+  $(event.currentTarget).closest('li').remove();
+}
+);
 
