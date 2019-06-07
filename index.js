9a561982fcf249537056a5ba0968e2a51f5a2d72
Colleen <colleenhiggins87@gmail.com>
2019-06-06 18:29:23 -0700

    changed acting event listeners to larger parent class to get the added list items to play nice

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
-// enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
-// check and uncheck items on the list by clicking the "Check" button
-// permanently remove items from the list
 'use strict';
+$(function(){
+//toggles with click of button to check off or uncheck off item
+  $('.shopping-list').on('click', '.shopping-item-toggle', event => {
+    event.preventDefault();
+    $(event.currentTarget).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
 
-/**
- * when the 'check' button is CLICKED it will add the class .shopping-item__checked to
- * the .shopping-item only for that item
- * if shopping item toggle is CLICKED and if shopping item checked is NOT there (on the shopping item),
- *  then (toggle) add class to check off item; if shopping item toggle is CLICKED and item is checked,
- * remove checked class from the item
- */
-
-$('.shopping-item-toggle').click(event => {
-  event.preventDefault();
-  console.log(event.currentTarget);
-  $(event.currentTarget).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-}
-);
-
-/**
- * add user text to list item when clicking 'Add item' button Or hitting return 
- * store user text and add that text to the list when you click button or hit return 
- */
-
-//when you click submit ... add text to list 
-$('#js-shopping-list-form').submit(event => {
-  event.preventDefault();
-  //this is the users text
-  const newEntry = $(event.currentTarget).find('#shopping-list-entry');
-  const testEntry = newEntry.val();
-  console.log(newEntry);
-
-
-  //adding user text to the list 
-  $('ul').append( 
-    `<li>
+  //when you click submit ... add text to list 
+  $('#js-shopping-list-form').submit(event => {
+    event.preventDefault();
+    //this is the users text
+    const newEntry = $(event.currentTarget).find('#shopping-list-entry');
+    const testEntry = newEntry.val();
+    //adding user text to the list 
+    $('.shopping-list').append( 
+      `<li>
   <span class="shopping-item">${testEntry}</span>
   <div class="shopping-item-controls">
     <button class="shopping-item-toggle">
@@ -45,21 +25,14 @@ $('#js-shopping-list-form').submit(event => {
     </button>
   </div>
 </li>`
-  );
+    );
+    //deleting the users text from the box
+    newEntry.val('');
+  });
+  //permanently remove items from the list
 
-  //deleting the users text from the box
-  newEntry.val('');
-
-});
-
-/**
- * permanently remove items from the list
- * this needs to occur at the li level so we need to select the parent of the delete button and then remove the whole thing
- */
-$('.shopping-item-delete').click(event => {
-  event.preventDefault();
-  const deleteItem = $(event.currentTarget).closest('.shopping-item');
-  $(deleteItem).remove();
-  
-});
-
+  $('.shopping-list').on('click', '.shopping-item-delete', event => {
+    event.preventDefault();
+    $(event.currentTarget).closest('li').remove();
+  });
+})
